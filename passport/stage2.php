ce7b56c23cfe553553dc8b45d3c722d43ddc95cd
IBTIDA <sk.muhtasimibtida@gmail.com>
2018-07-06 16:00:26 +0600

    test 2

--- passport/stage2.php
+++ passport/stage2.php
@@ -4,7 +4,7 @@
 	<title>Passport Stage2</title>
 	<style>
 		table, th, td {
-    border: 1 px solid black;
+    border: 1px solid black;
     border-collapse: collapse;
 }
 	</style>
@@ -14,5 +14,27 @@
      <p align="center">Online Application ID:</p>
      <p align="center">Filed marked with <b style="color: red">(*)</b> are mandatory</p>
      <hr/>
+     <table>
+     	<form>
+     		<tr>
+     		<td rowspan="100" width="150 px"></td>
+     		<td colspan="3"><h3>Applicant Contact Information</h3></td>
+     		<td rowspan="100" width="150 px"></td>
+     		<td colspan="3"><h3>Old Passport Information</h3></td>
+     		
+     		
+     	</tr>
+
+     	<tr>
+     		<td><label>Office No:</label></td>
+     		<td width="40 px"></td>
+     		<td><input style="width: 300px" type="id" id="ONum" placeholder="Office Number"></td>
+     		
+     		<td><label>Passport No:</label></td>
+     		<td width="40 px"></td>
+     		<td><input style="width: 300px" type="id" id="PNum" placeholder="Passport Number"></td>
+     	</tr>
+     	</form>
+     </table>
 </body>
 </html>
