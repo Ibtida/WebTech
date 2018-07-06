402a46d855765da756d81afa5369e2e7e91673a5
IBTIDA <sk.muhtasimibtida@gmail.com>
2018-07-06 15:27:51 +0600

    stage 2 1st

---
+++ passport/stage2.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Passport Stage2</title>
+	<style>
+		table, th, td {
+    border: none;
+    border-collapse: collapse;
+}
+	</style>
+</head>
+<body>
+
+</body>
+</html>
