b94f8d6c0b02ace9b5852d999dd0bac3eb33009e
IBTIDA <sk.muhtasimibtida@gmail.com>
2018-07-06 15:36:18 +0600

    again test

--- passport/stage2.php
+++ passport/stage2.php
@@ -4,12 +4,15 @@
 	<title>Passport Stage2</title>
 	<style>
 		table, th, td {
-    border: none;
+    border: 1 px solid black;
     border-collapse: collapse;
 }
 	</style>
 </head>
 <body>
-
+	<h2 align="center">PASSPORT APPLICATION - STAGE 2</h2>
+     <p align="center">Online Application ID:</p>
+     <p align="center">Filed marked with <b style="color: red">(*)</b> are mandatory</p>
+     <hr/>
 </body>
 </html>
